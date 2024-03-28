d5c4c25bfa8107202b67af358ce2e8674ee739e2
Madavaram Yethishwar <149243728+MYethishwar@users.noreply.github.com>
2024-03-28 12:33:36 +0530

    Added on 28_03_2024

---
+++ Basic Programs/TpeCast.java
@@ -0,0 +1,13 @@
+class TypeCast{
+	public static void main(String[] args){
+		int marks = 80;
+		long lmv = marks;
+		System.out.println("integer value"+marks);
+		System.out.println("long value"+lmv);
+
+		float d = 12.5 ;
+		double inm = (double)12.5;
+		System.out.println("After Type Casting"+inm);
+
+	}
+}
