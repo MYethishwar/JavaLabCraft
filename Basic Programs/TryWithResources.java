860fc10b92b9672e84578f113d0bb15b15f779f9
Madavaram Yethishwar <149243728+MYethishwar@users.noreply.github.com>
2024-06-22 15:39:50 +0530

    Added TryWithResouces Program to 22-06-24 Lab Programs

---
+++ Basic Programs/TryWithResources.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class TryWithResources {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter nr: ");
+			int nr = sc.nextInt();
+			
+			System.out.println("Enter dr: ");
+			int dr = sc.nextInt();
+
+			int result = nr/dr;
+			System.out.println("Result of division: " + result);
+			} 
+			catch (ArithmeticException e) {
+				System.out.println("Error: Division by zero is not allowed.");
+			}
+			catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+}
